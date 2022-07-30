1a7863f300a701e1af58d73f70a8c063072a6aff
evanzha <ezpeasy2007@gmail.com>
2022-07-30 18:29:05 -0400

    changed cube storage from int to char, modified printcube( ), renamed fcw( ) --> greencw( ), added new functions and modified greencw( ) for simplicity

--- projects/Code Function list/Source.cpp
+++ projects/Code Function list/Source.cpp
@@ -6,21 +6,13 @@ using namespace std;
 
 class Rubik {
 private:
-	/*
-		0 --> white face - UP
-		1 --> orange face - LEFT
-		2 --> green face - FRONT
-		3 --> red face - RIGHT
-		4 --> blue face - BACK
-		5 --> yellow face - DOWN
-	*/
-	int cube[6][3][3] = {
-		{{ -1, -1, -1 }, { -1, 0, -1 }, { -1, -1, -1 }},	// white face --> top, middle, bottom row
-		{{ -1, -1, -1 }, { -1, 1, -1 }, { -1, -1, -1 }},	// orange face --> top, middle, bottom row
-		{{ -1, -1, -1 }, { -1, 2, -1 }, { -1, -1, -1 }},	// green face --> top, middle, bottom row
-		{{ -1, -1, -1 }, { -1, 3, -1 }, { -1, -1, -1 }},	// red face --> top, middle, bottom row
-		{{ -1, -1, -1 }, { -1, 4, -1 }, { -1, -1, -1 }},	// blue face --> top, middle, bottom row
-		{{ -1, -1, -1 }, { -1, 5, -1 }, { -1, -1, -1 }}		// yellow face --> top, middle, bottom row
+	char cube[6][3][3] = {
+		{{ ' ', ' ', ' ' }, { ' ', 'y', ' ' }, { ' ', ' ', ' ' }},
+		{{ ' ', ' ', ' ' }, { ' ', 'r', ' ' }, { ' ', ' ', ' ' }},
+		{{ ' ', ' ', ' ' }, { ' ', 'g', ' ' }, { ' ', ' ', ' ' }},
+		{{ ' ', ' ', ' ' }, { ' ', 'o', ' ' }, { ' ', ' ', ' ' }},
+		{{ ' ', ' ', ' ' }, { ' ', 'b', ' ' }, { ' ', ' ', ' ' }},
+		{{ ' ', ' ', ' ' }, { ' ', 'w', ' ' }, { ' ', ' ', ' ' }}
 	};
 
 public:
@@ -32,104 +24,127 @@ public:
 	}
 
 	void printCube() {
-		for (int i = 0; i < 6; i++)
-		{
+		cout << "==============================" << endl;
+		//        c c c
+		//c c c   c c c  c c c  c c c
+		for (int i = 0; i < 3; i++) {
+			cout << "        ";
 			for (int j = 0; j < 3; j++)
-			{
-				for (int k = 0; k < 3; k++)
-					cout << cube[i][j][k] << " ";
-				cout << endl;
+				cout << cube[0][i][j] << " ";
+			cout << endl;
+		}
+		cout << endl;
+
+		for (int i = 0; i < 3; i++) {
+			for (int c = 1; c <= 4; c++) {
+				for (int j = 0; j < 3; j++) {
+					cout << cube[c][i][j] << " ";
+				}
+				cout << "  ";
 			}
+			cout << endl;
+		}
+		cout << endl;
+
+		for (int i = 0; i < 3; i++) {
+			cout << "        ";
+			for (int j = 0; j < 3; j++)
+				cout << cube[5][i][j] << " ";
 			cout << endl;
 		}
 	}
 
-// private:
-	// green face is front, white face is top
-	void fcw() { // Front
-		array<int, 8> face = {
-			cube[2][0][0],
-			cube[2][0][1],
-			cube[2][0][2],
-			cube[2][1][2],
-			cube[2][2][2],
-			cube[2][2][1],
-			cube[2][2][0],
-			cube[2][1][0]
-		};
-		array<int, 12> side = {
+	void greencw() {
+		clockwise90(
+			cube[2][0][0], cube[2][0][1], cube[2][0][2],
+			cube[2][1][0], cube[2][1][2],
+			cube[2][2][0], cube[2][2][1], cube[2][2][2],
+
 			cube[0][2][0], cube[0][2][1], cube[0][2][2],
 			cube[3][0][0], cube[3][1][0], cube[3][2][0],
 			cube[5][0][2], cube[5][0][1], cube[5][0][0],
 			cube[1][2][2], cube[1][1][2], cube[1][0][2]
-		};
-
-		std::rotate(face.begin(), face.begin() + 5, face.end());
-		std::rotate(side.begin(), side.begin() + 9, side.end());
-
-		cube[2][0][0] = face[0];
-		cube[2][0][1] = face[1];
-		cube[2][0][2] = face[2];
-		cube[2][1][2] = face[3];
-		cube[2][2][2] = face[4];
-		cube[2][2][1] = face[5];
-		cube[2][2][0] = face[6];
-		cube[2][1][0] = face[7];
-
-		cube[0][2][0] = side[0]; cube[0][2][1] = side[1]; cube[0][2][2] = side[2];
-		cube[3][0][0] = side[3]; cube[3][1][0] = side[4]; cube[3][2][0] = side[5];
-		cube[5][0][2] = side[6]; cube[5][0][1] = side[7]; cube[5][0][0] = side[8];
-		cube[1][2][2] = side[9]; cube[1][1][2] = side[10]; cube[1][0][2] = side[11];
+		);
 	}
-	void fccw() { // Front Prime
-		fcw(); fcw(); fcw();
-	}
-	void bcw() { // Bottom
+	void greenccw() {
+		counterclockwise90(
+			cube[2][0][0], cube[2][0][1], cube[2][0][2],
+			cube[2][1][0], cube[2][1][2],
+			cube[2][2][0], cube[2][2][1], cube[2][2][2],
 
-	}
-	void bccw() { // Bottom Prime
-		bcw(); bcw(); bcw();
+			cube[0][2][0], cube[0][2][1], cube[0][2][2],
+			cube[3][0][0], cube[3][1][0], cube[3][2][0],
+			cube[5][0][2], cube[5][0][1], cube[5][0][0],
+			cube[1][2][2], cube[1][1][2], cube[1][0][2]
+		);
 	}
 
-	void ucw() { // Up
-		
-	}
-	void uccw() { // Up Prime
-		ucw(); ucw(); ucw();
-	}
-	void dcw() { // Down
-		
-	}
-	void dccw() { // Down Prime
-		dcw(); dcw(); dcw();
+private:
+	void rotate(char& a, char& b, char& c, char& d) {
+		char tmp = a;
+		a = b;
+		b = c;
+		c = d;
+		d = tmp;
 	}
 
-	void rcw() { // Right
+	void clockwise90(
+		char& faceTL, char& faceTC, char& faceTR,
+		char& faceCL, char& faceCR,
+		char& faceBL, char& faceBC, char& faceBR,
 
-	}
-	void rccw() { // Right Prime
-		rcw(); rcw(); rcw();
-	}
-	void lcw() { // Left
+		char& sideTL, char& sideTC, char& sideTR,
+		char& sideRT, char& sideRC, char& sideRB,
+		char& sideBR, char& sideBC, char& sideBL,
+		char& sideLB, char& sideLC, char& sideLT
+		)
+	{
+		// face switching
+		rotate(faceTL, faceBL, faceBR, faceTR);
+		rotate(faceTC, faceCL, faceBC, faceCR);
 
+		// side switching
+		rotate(sideTL, sideLB, sideBR, sideRT);
+		rotate(sideTC, sideLC, sideBC, sideRC);
+		rotate(sideTR, sideLT, sideBL, sideRB);
 	}
-	void lccw() { // Left Prime
-		lcw(); lcw(); lcw();
+	void counterclockwise90(
+		char& faceTL, char& faceTC, char& faceTR,
+		char& faceCL, char& faceCR,
+		char& faceBL, char& faceBC, char& faceBR,
+
+		char& sideTL, char& sideTC, char& sideTR,
+		char& sideRT, char& sideRC, char& sideRB,
+		char& sideBR, char& sideBC, char& sideBL,
+		char& sideLB, char& sideLC, char& sideLT
+		)
+	{
+		// face switching
+		rotate(faceTL, faceTR, faceBR, faceBL);
+		rotate(faceTC, faceCR, faceBC, faceCL);
+
+		// side switching
+		rotate(sideTL, sideRT, sideBR, sideLB);
+		rotate(sideTC, sideRC, sideBC, sideLC);
+		rotate(sideTR, sideRB, sideBL, sideLT);
 	}
 };
 
 int main()
 {
 	int faces[6][3][3] = {
-		{{ 0, 0, 0 }, { 0, 0, 0 }, { 0, 0, 0 }},	// white face --> top, middle, bottom row
-		{{ 1, 1, 1 }, { 1, 1, 1 }, { 1, 1, 1 }},	// green face --> top, middle, bottom row
-		{{ 2, 2, 2 }, { 2, 2, 2 }, { 2, 2, 2 }},	// orange face --> top, middle, bottom row
-		{{ 3, 3, 3 }, { 3, 3, 3 }, { 3, 3, 3 }},	// blue face --> top, middle, bottom row
-		{{ 4, 4, 4 }, { 4, 4, 4 }, { 4, 4, 4 }},	// red face --> top, middle, bottom row
-		{{ 5, 5, 5 }, { 5, 5, 5 }, { 5, 5, 5 }}		// yellow face --> top, middle, bottom row
+		{{ 'g', 'r', 'r' }, { 'g', 'y', 'y' }, { 'g', 'y', 'y' }},
+		{{ 'w', 'w', 'w' }, { 'o', 'r', 'r' }, { 'o', 'r', 'r' }},
+		{{ 'o', 'g', 'g' }, { 'y', 'g', 'g' }, { 'y', 'b', 'b' }},
+		{{ 'o', 'o', 'y' }, { 'o', 'o', 'y' }, { 'r', 'r', 'y' }},
+		{{ 'b', 'b', 'r' }, { 'b', 'b', 'w' }, { 'b', 'b', 'w' }},
+		{{ 'g', 'w', 'w' }, { 'g', 'w', 'w' }, { 'b', 'o', 'o' }}
 	};
 	Rubik rubik(faces);
-	rubik.fcw();
+	rubik.printCube();
+	rubik.greencw();
+	rubik.printCube();
+	rubik.greenccw();
 	rubik.printCube();
 
 	return 0;
